6138b61245ca86ba1ad6f2329231ad2b86915189
Aman Sharma <amansharma1829@gmail.com>
2024-10-14 10:42:15 +0530

    Added changes for topo

--- graph/course_schedule.cpp
+++ graph/course_schedule.cpp
@@ -10,6 +10,24 @@ using namespace std;
 #define debug3(x,y,z) cout<<#x<<" :: "<<x<<"\t"<<#y<<" :: "<<y<<"\t"<<#z<<" :: "<<z<<endl;
 #define debug4(p,q,r,s) cout<<#p<<" :: "<<p<<"\t"<<#q<<" :: "<<q<<"\t"<<#r<<" :: "<<r<<"\t"<<#s<<" :: "<<s<<endl;
 
+ll start, endi;
+
+bool dfs(ll src, vector<vector<ll>> &adj, vector<char> &color) {
+    color[src] = 1;
+    for(auto v : adj[src]) {
+        if(color[v] == 0 && dfs(v, adj, color)) {
+            return true;
+        } else if(color[v] == 1) {
+            // Marking the correct start and end is important
+            start = v;
+            endi = src;
+            return true;
+        }
+    }
+    color[src] = 2;
+    return false;
+}
+
 void topoSort(ll src, stack<ll> &st, vector<vector<ll>> &adj, vector<bool> &vis) {
     vis[src] = true;
     for(auto v : adj[src]) {
@@ -34,14 +52,24 @@ int main() {
     }
     stack<ll> st;
     vector<bool> vis(n + 2, false);
+    vector<char> color(n + 2, 0);
+    start = -1;
     for(ll i = 1; i <= n; i++) {
-        if(!vis[i])
-            topoSort(i, st, adj, vis);
+        if(color[i] == 0 && dfs(i, adj, color))
+            break;
     }
-    while(!st.empty()) {
-        cout << st.top() << " ";
-        st.pop();
+    if(start == -1) {
+        for(ll i = 1; i <= n; i++) {
+            if(!vis[i])
+                topoSort(i, st, adj, vis);
+        }
+        while(!st.empty()) {
+            cout << st.top() << " ";
+            st.pop();
+        }
+        cout << endl;
+    } else {
+        cout << "IMPOSSIBLE" << endl;
     }
-    cout << endl;
     return 0;
 }
